f47ef4c8bc4c39a17fd6f7b5111409142b6d65d7
Annika Ehlers <e_hlers@live.dk>
2019-11-13 23:11:06 +0100

    Log in functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import facade from "./apifacade";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [roles, setRoles] = useState();
+
+  const logout = () => {
+    facade.logout();
+    setLoggedIn(false);
+  }
+
+  const login = (user, pass) => {
+    facade.login(user, pass).then(res => setLoggedIn(true));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!loggedIn ? (<LogIn login={login} />) :
+        (<div>
+          <LoggedIn roles={roles} />
+          <button onClick={logout}>Logout</button>
+        </div>)}
     </div>
-  );
+  )
+}
+
+function LogIn(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = (event) => {
+    event.preventDefault();
+    props.login(username, password);
+  }
+
+  const onChange = (event) => {
+    if (event.target.id === "username") {
+      setUsername(event.target.value);
+    } else {
+      setPassword(event.target.value);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={login} onChange={onChange} >
+        <input placeholder="User Name" id="username" />
+        <input placeholder="Password" id="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  )
+}
+
+function LoggedIn(props) {
+  const [dataFromServer, setDataFromServer] = useState("Fetching!!");
+  return (
+    <div>
+      <h2>Data recieved</h2>
+      <h3>{dataFromServer}</h3>
+
+    </div>
+  )
 }
 
 export default App;
