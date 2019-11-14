7057fd91e2fa1d4d89e436ff4cc0ef1d4cded1a6
Annika Ehlers <e_hlers@live.dk>
2019-11-14 12:48:37 +0100

    Started styling

--- src/App.js
+++ src/App.js
@@ -53,16 +53,41 @@ function LogIn(props) {
   }
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={login} onChange={onChange} >
-        <input placeholder="User Name" id="username" />
-        <input placeholder="Password" id="password" />
-        <button>Login</button>
+
+    <div className="container container-small">
+    <div className="wrapper">
+    <h2>Login</h2><br/>
+      <form className="form-signin" onSubmit={login} onChange={onChange} >
+        <div className="form form-group">
+        <input className="form-control" placeholder="User Name" id="username" />
+        </div><div className="form-group">
+        <input className="form-control" placeholder="Password" id="password" /> <br/>
+        <button className="btn btn-primary">Login</button>
+        </div>
       </form>
+      </div>
     </div>
   )
 }
+
+/*
+form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+*/
 
 function LoggedIn(props) {
   const {roles} = props;
