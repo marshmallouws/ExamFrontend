222ed57b5bc4262207d3f906fd0534011ff168e7
Annika Ehlers <e_hlers@live.dk>
2019-11-15 20:10:31 +0100

    Redirect when logging in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react"
 import facade from "./apifacade";
+import Navbar from "./Navbar";
+import Login from "./Login";
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,12 +14,9 @@ import {
 function App() {
   const [roles, setRoles] = useState([]);
 
-
   const logInState = (r) => {
     setRoles(r);
   }
-
-
 
   return (
     <Router>
@@ -27,50 +26,11 @@ function App() {
           <Home />
         </Route>
         <Route path="/login">
-          <LogIn logInState={logInState} />
+          <Login logInState={logInState} />
         </Route>
         <PrivateRoute path="/user" component={LoggedIn} roles={roles} />
       </Switch>
     </Router>
-  );
-}
-
-function Navbar(props) {
-  const [isAuthenticating, setIsAuthenticating] = useState(true);
-  const [isAuthenticated, userHasAuthenticated] = useState(false);
-
-  const logout = () => {
-    facade.logout();
-    userHasAuthenticated(false);
-  }
-
-  useEffect(() => {
-    onLoad();
-  }, []);
-
-  function onLoad() {
-    userHasAuthenticated(true);
-    setIsAuthenticating(false);
-  }
-
-  return (
-    !isAuthenticating &&
-    <nav className="header">
-      <ul>
-        <li>
-          <NavLink exact to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/user">Test</NavLink>
-        </li>
-
-        {facade.getToken() == null ? (<li style={{ float: "right" }}>
-          <Link to="/login">Log in</Link>
-        </li>) : (<li style={{ float: "right" }}><Link to="/" onClick={logout}>Log out</Link></li>)
-        }
-
-      </ul>
-    </nav>
   );
 }
 
@@ -88,46 +48,6 @@ function Home() {
   return (
     <div>Home</div>
   );
-}
-
-function LogIn(props) {
-  const [err, setErr] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const login = (event) => {
-    event.preventDefault();
-    facade.login(username, password)
-      .then(data => { props.logInState(data.roles) })
-      .catch(err => {
-        setErr("Wrong username or password");
-      });
-  }
-
-  const onChange = (event) => {
-    setErr("");
-    if (event.target.id === "username") {
-      setUsername(event.target.value);
-    } else {
-      setPassword(event.target.value);
-    }
-  }
-
-  return (
-    <div className="container container-small">
-      <div className="wrapper">
-        <h2>Login</h2><br />
-        <form className="form-signin" onSubmit={login} onChange={onChange} >
-          <div className="form form-group">
-            <input className="form-control" placeholder="User Name" id="username" />
-          </div><div className="form-group">
-            <input className="form-control" placeholder="Password" id="password" /> <br />
-            <button className="btn btn-primary">Login</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  )
 }
 
 function LoggedIn(props) {
